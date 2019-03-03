6b7fd88c1bb4489b39e0069332498eed792570d8
Nic Bathgate <balinornz@gmail.com>
2019-03-03 23:09:11 +1300

    basic styling store stories and subtasks separately in state display asignees of subtasks alongside stories add a handy function to create css class names from strings

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import APIURL from "./ApiURL";
 import { RouteComponentProps } from "react-router";
-import { Issue } from "./JiraInterfaces";
+import { Issue, Story, SubTask } from "./JiraInterfaces";
 
 interface BoardState {
   stories: Issue[];
-  subTasks: Issue[];
+  subtasks: Issue[];
 }
 
 interface BoardRouterProps {
@@ -14,21 +14,14 @@ interface BoardRouterProps {
 interface BoardProps extends RouteComponentProps<BoardRouterProps> {}
 
 class Board extends Component<BoardProps, BoardState> {
-  state = { stories: [], subTasks: [] };
+  state = { stories: [], subtasks: [] };
   componentDidMount() {
     fetch(`${APIURL}/board/${this.props.match.params.id}`, {
       method: "get"
     })
       .then(res => res.json())
-      .then(issues => {
-        const stories = issues.filter(
-          (story: Issue) => story.fields.issuetype.name === "Story"
-        );
-        // TODO: collate the subtasks from inside the stories
-        const subTasks = issues.filter(
-          (story: Issue) => story.fields.issuetype.name === "Sub-Task"
-        );
-        this.setState({ stories, subTasks });
+      .then(({ stories, subtasks }) => {
+        this.setState({ stories, subtasks });
       });
   }
   render() {
@@ -38,12 +31,31 @@ class Board extends Component<BoardProps, BoardState> {
           Stories for Board {this.props.match.params.id} ({
             this.state.stories.length
           }{" "}
-          stories, {this.state.subTasks.length} sub-tasks)
+          stories, {this.state.subtasks.length} sub-tasks)
         </h2>
         {this.state.stories.length === 0 && <div>Loading...</div>}
         <ul>
-          {this.state.stories.map((story: Issue) => (
-            <li key={story.id}>{story.fields.summary}</li>
+          {this.state.stories.map((story: Story) => (
+            <li
+              className={`status ${slugify(story.fields.status.name)}`}
+              key={story.id}
+            >
+              {story.fields.summary}
+              <br />
+              {story.fields.epic && story.fields.epic.name} {story.key}{" "}
+              {story.fields.customfield_10806} {story.fields.priority.name}
+              <img src={story.fields.assignee.avatarUrls["48x48"]} />
+              {story.fields.subtasks.map(subtaskRef => {
+                const subtask: any = this.state.subtasks.find(
+                  (s: SubTask) => s.id === subtaskRef.id
+                );
+                console.log(subtask);
+                if (subtask.fields.assignee)
+                  return (
+                    <img src={subtask.fields.assignee.avatarUrls["48x48"]} />
+                  );
+              })}
+            </li>
           ))}
         </ul>
       </div>
@@ -52,3 +64,19 @@ class Board extends Component<BoardProps, BoardState> {
 }
 
 export default Board;
+
+function slugify(string: string) {
+  const a = "àáäâãåèéëêìíïîòóöôùúüûñçßÿœæŕśńṕẃǵǹḿǘẍźḧ·/_,:;";
+  const b = "aaaaaaeeeeiiiioooouuuuncsyoarsnpwgnmuxzh------";
+  const p = new RegExp(a.split("").join("|"), "g");
+  return string
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with
+    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+    .replace(/&/g, "-and-") // Replace & with ‘and’
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters
+    .replace(/\-\-+/g, "-") // Replace multiple — with single -
+    .replace(/^-+/, "") // Trim — from start of text
+    .replace(/-+$/, ""); // Trim — from end of text
+}
--- src/JiraInterfaces.tsx
+++ src/JiraInterfaces.tsx
@@ -23,29 +23,119 @@ export interface IssueList {
   total: number;
   issues: Issue[];
 }
+export interface StoryList {
+  expand: string;
+  startAt: number;
+  maxResults: number;
+  total: number;
+  issues: Story[];
+}
+export interface SubtaskList {
+  expand: string;
+  startAt: number;
+  maxResults: number;
+  total: number;
+  issues: SubTask[];
+}
 
 export interface Issue {
   id: string;
   self: string;
   key: string;
-  fields: {
-    issuetype: { name: string };
-    epic: {
-      id: number;
-      key: string;
-      self: string;
-      name: string;
-      summary: string;
-      done: boolean;
-    };
-    status: {
-      self: string;
-      name: string;
-    };
+  fields: IssueFields;
+}
+export interface Story {
+  id: string;
+  self: string;
+  key: string;
+  fields: StoryFields;
+}
+export interface SubTask {
+  id: string;
+  key: string;
+  self: string;
+  fields: SubtaskFields;
+}
+interface IssueFields {
+  issuetype: IssueType;
+  status: Status;
+  summary: string;
+  description: string;
+  assignee: Actor;
+  created?: Date;
+  creator: Actor;
+  labels?: string[];
+  priority: Priority;
+  reporter: Actor;
+  updated?: Date;
+}
+interface StoryFields extends IssueFields {
+  epic: {
+    id: number;
+    key: string;
+    self: string;
+    name: string;
     summary: string;
-    description: string;
-    customfield_10806: number;
+    done: boolean;
   };
+  closedSprints: [];
+  customfield_10806: number;
+  customfield_11400?: Actor[];
+  subtasks: SubTask[];
+}
+interface SubtaskFields extends IssueFields {
+  resolutiondate?: Date;
+  resolution?: { name: string };
+  parent?: Issue;
+}
+
+export interface Sprint {
+  completeDate?: Date;
+  endDate?: Date;
+  startDate?: Date;
+  goal: string;
+  id: number;
+  name: string;
+  originBoardId: number;
+  self: string;
+  state: string;
+}
+
+interface Actor {
+  accountId: string;
+  active: boolean;
+  avatarUrls: {
+    "16x16": string;
+    "24x24": string;
+    "32x32": string;
+    "48x48": string;
+  };
+  displayName: string;
+  emailAddress: string;
+  name: string;
+  timeZone: string;
+}
+
+interface Status {
+  id: string;
+  iconUrl: string;
+  name: string;
+  self: string;
+}
+
+interface Priority {
+  id: string;
+  name: string;
+  self: string;
+  iconUrl: string;
+}
+
+interface IssueType {
+  id: string;
+  name: string;
+  iconUrl: string;
+  self: string;
+  subtask: boolean;
 }
 
 export interface EpicList {
