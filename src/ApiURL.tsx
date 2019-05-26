03baf945d9324a4cb03ca9c41c237a9ebadde854
Nic Bathgate <n.bathgate@adinstruments.com>
2019-05-27 09:30:30 +1200

    Merge branch 'master' of C:\dev\scrum-board with conflicts.

--- src/ApiURL.tsx
+++ src/ApiURL.tsx
@@ -1,3 +1,4 @@
-const APIURL: string =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8080/api";
+// const APIURL: string =
+//   process.env.NODE_ENV === "production" ? "/api" : "http://localhost:8080";
+const APIURL: string = "http://nbathgate.adi.co.nz:8080";
 export default APIURL;
